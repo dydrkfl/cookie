6f09f277f06ed28afead9ddb0a10da853ae5cb3d
= <qqfelix587@gmail.com>
2020-10-21 21:59:13 +0900

    lec10 Acesss Control

--- main.js
+++ main.js
@@ -76,6 +76,10 @@ var app = http.createServer(function (request, response) {
       });
     }
   } else if (pathname === '/create') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     fs.readdir('./data', function (error, filelist) {
       var title = 'WEB - create';
       var list = template.list(filelist);
@@ -95,6 +99,10 @@ var app = http.createServer(function (request, response) {
       response.end(html);
     });
   } else if (pathname === '/create_process') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     var body = '';
     request.on('data', function (data) {
       body = body + data;
@@ -111,6 +119,10 @@ var app = http.createServer(function (request, response) {
       })
     });
   } else if (pathname === '/update') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     fs.readdir('./data', function (error, filelist) {
       var filteredId = path.parse(queryData.id).base;
       fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
@@ -138,6 +150,10 @@ var app = http.createServer(function (request, response) {
       });
     });
   } else if (pathname === '/update_process') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     var body = '';
     request.on('data', function (data) {
       body = body + data;
@@ -157,6 +173,10 @@ var app = http.createServer(function (request, response) {
       });
     });
   } else if (pathname === '/delete_process') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     var body = '';
     request.on('data', function (data) {
       body = body + data;
@@ -191,6 +211,10 @@ var app = http.createServer(function (request, response) {
     });
 
   } else if (pathname === '/login_process') {
+    if(authIsOwner(request, response) ===false){
+      response.end('Login required!!');
+      return false;
+    }
     var body = '';
     request.on('data', function (data) {
       body = body + data;
