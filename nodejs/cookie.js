ed3e115da527defe2c0c4f44f5c11516307c2ab2
= <qqfelix587@gmail.com>
2020-10-21 18:34:59 +0900

    lec4 Secure&HttpOnly cookie

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -11,8 +11,12 @@ http.createServer(function(request, response){
         'Set-Cookie' :[
             'yummy_cookie = choco',
             // 이러한 형태의 쿠키는 session cookie로 사용자가 브라우저를 닫으면 사라짐.
-            `Permanent=cookies; Max-age=${60*60*24*30}`
+            `Permanent=cookies; Max-age=${60*60*24*30}`,
             // permanent cookie는 host가 설정한 expire date까지 지속적으로 남아있음.
+            'Secure=Secure; Secure',
+            // https일때만 request하게 함 -> http를 쓸 경우 cookie를 해킹하기 용이하므로
+            'HttpOnly=HttpOnly; HttpOnly'
+            // java script로 쿠키를 해킹하는 경우가 많으므로 이것 역시 유용함.
         ]
     })
     response.end('Cookie!!')
