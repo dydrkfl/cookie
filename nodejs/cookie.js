3fa465193dcb01f83a38df62ccb5605a2955ebc7
= <qqfelix587@gmail.com>
2020-10-21 18:07:20 +0900

    lec2 read cookies

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response){
-    response.writeHead(200, {
-        'Set-Cookie' : ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    })
+    console.log(request.headers.cookie)
+    if(request.headers.cookie !== undefined){
+        var cookies = cookie.parse(request.headers.cookie);
+        console.log(cookies.yummy_cookie);
+    }
     response.end('Cookie!!')
 }).listen(3000);
